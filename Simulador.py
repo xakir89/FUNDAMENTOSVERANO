4a16f3b93f7063faa9726c434153c77d0d6d0a02
Anderson Lopez <xakir89@gmail.com>
2026-07-10 10:02:53 -0500

    Update Simulador.py
    quite variables que no estaba usando, cree un menu  y cambie un error que habia en variables para poder hacer un  ajuste y que la inforamcion que le envio se atualice correctamente

--- Simulador.py
+++ Simulador.py
@@ -1,51 +1,71 @@
-compuertas = ['A','P','R','A','P','R']
-e0 = float(input("Eneria inicial en Joules (J): "))
-Eimp = float(input("Energia de impulso: "))
-Efreno = float(input("Energia de Freno: "))
+def operaciones():
+    compuertas = ['A','P','R','A','P','R']
+    e0 = float(input("Eneria inicial en Joules (J): "))
+    Eimp = float(input("Energia de impulso: "))
+    Efreno = float(input("Energia de Freno: "))
 
-particula_viva = True
-motivo_parada = ""
-compuerta_actual = 0
+    particula_viva = True
+    compuerta_actual = 0
+    Energia_actual = e0
 
-while compuerta_actual < len(compuertas) and particula_viva:
-    tipo = compuertas[compuerta_actual]
-    compuerta_actual = compuerta_actual + 1
-    num_paso = compuerta_actual + 1
+    while compuerta_actual < len(compuertas) and particula_viva:
+        tipo = compuertas[compuerta_actual]
+        compuerta_actual = compuerta_actual + 1
+        
 
-    print(f"\n[Compuerta #{num_paso} - {tipo}]")
-    print(f" > ENERGIA AL ENTRAR: {e0:.2f}")
-
-    if tipo =='A':      
-# COMPUERTA TIPO A (ACELERADORA)
-        if 0 < e0 < 15:
-            Energia_actual = e0+(Eimp * 0.5)
-            print(f"Perdida de Eficiencia del 50% {Energia_actual}")
-        elif e0 > 15:
-            Energia_actual = e0 + Eimp
-            print(f"Incrementa la Energia de la Particula {Energia_actual}")
-        else:
-            print("La particula se quedo sin energia y para completamente")
-            break
-# COMPUERTA TIPO P (PASIVA / FRENO)
-    elif tipo == 'P':
-        if 1.0 <= Energia_actual <= 5.0:
-            print("Zona de Resonancia Destructiva no se puede avanzar")
-            break
-        else: 
+        print(f"\n[Compuerta #{compuerta_actual} - {tipo}]")
+        print(f"\n> ENERGIA AL ENTRAR: {Energia_actual:.2f}")
+        
+        if tipo =='A':      
+    # COMPUERTA TIPO A (ACELERADORA)
+            if 0 < Energia_actual < 15:
+                Energia_actual = Energia_actual+(Eimp * 0.5)
+                print(f"Perdida de Eficiencia del 50% {Energia_actual:.2f}")
+            elif e0 > 15:
+                Energia_actual = Energia_actual + Eimp
+                print(f"Incrementa la Energia de la Particula {Energia_actual:.2f}")
+            else:
+                print("La particula no tiene energia o es igual a 0, para completamente")
+                particula_viva = False
+                
+    # COMPUERTA TIPO P (PASIVA / FRENO)
+        elif tipo == 'P':
             Energia_actual = Energia_actual - Efreno
-            print(f"Enerigia atual menos la Energia de Freno: {Energia_actual}")
-# COMPUERTA TIPO R (RESONANTE)
-    elif tipo == 'R':
-        if Energia_actual % 2 == 0:
-            amplificacion = Energia_actual * 1.2
-            print(f"factor de Amplificacion: {amplificacion}")
+            print(f"Enerigia atual menos la Energia de Freno: {Energia_actual:.2f}")
+            if 1.0 <= Energia_actual <= 5.0:
+                print("Zona de Resonancia Destructiva no se puede avanzar particula atrapada en el intervalo (1.0 a 5.0)")
+                particula_viva = False            
+    # COMPUERTA TIPO R (RESONANTE)
+        elif tipo == 'R':
+            if int(Energia_actual) % 2 == 0:
+                Energia_actual = Energia_actual * 1.2
+                print(f"factor de Amplificacion: {Energia_actual:.2f}")
+            else:
+                Energia_actual = Energia_actual - 5.0
+                print(f"Penalizacion - 5.0 J: {Energia_actual:.2f}")
+
+def menu():
+    while True:
+        print("""
+*******************************************
+*   Simulador de Filtrado de Partículas   *
+*    en Acelerador Magnético Secuencial   *
+*                                         *
+*    1. ingresar valores solicitadios     *             
+*    2. Salir                             *
+*                                         *
+*******************************************
+              """)      
+        opc = int(input("Ingrese Opcion: "))
+        if opc == 1:
+            operaciones()
+        elif opc == 2:
+            print("""
+Gracias por confiar en nuestros servicios
+    Contactanos y solicita tu software
+        Whatsapp: 301 553 0836
+                  """)
+            break
         else:
-            Energia_actual = Energia_actual - 5.0
-            print(f"Penalizacion - 5.0 J: {Energia_actual}")
-    
-
-# Parada y Fallo del Sistema 
-
-#
-
-    
+            print("Dato errado solo puede ser 1 o 2")
+menu()
