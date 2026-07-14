8d6258fa5477c0259802e5f5322d9685aa8dcd9c
Anderson Lopez <xakir89@gmail.com>
2026-07-14 16:46:57 -0500

    Update Problema 2.1.py

--- Problema 2.1.py
+++ Problema 2.1.py
@@ -1,18 +1,17 @@
 coste_original = 20000
 vida_util = 6
 valor_recuperacion = 2000
+depreciacion_acumulada = 0
 anio = 2026
-
-for i in range(1,vida_util + 1  ):
-    d = (coste_original - valor_recuperacion)/i
-    print(f"Fila | Año  | Depreciación | Depreciación Acumulada | Valor Anual ")
-    print()
-    print(f" {i}   | {anio} |     2000      |        {valor_recuperacion}          |    {d}")
-    print()
-    coste_original = d
-    valor_recuperacion += 2000
-    
-    
+d = (coste_original - valor_recuperacion) / vida_util
+print()
+print(f"Fila | Año  | Depreciación | Depreciación Acumulada | Valor Anual ")
+print()
+for i in range(1,vida_util+1):
+    depreciacion_acumulada += d
+    valor_anual = coste_original - depreciacion_acumulada
+    print(f" {i}   | {anio} |   {d}    |         {depreciacion_acumulada}          |    {valor_anual}")
+    print() 
     anio += 1
     
 
