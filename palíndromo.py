a0386e8466c4e357ba50e917cdf295678c7b8588
Anderson Lopez <xakir89@gmail.com>
2026-07-16 21:31:06 -0500

    e

---
+++ palíndromo.py
@@ -0,0 +1,25 @@
+def longitud(texto):
+    cont = 0
+    for letra in texto:
+        cont += 1
+    return cont
+def reconocer(palabra):
+    inicio = 0
+    fin = longitud(palabra) - 1
+    while inicio < fin:
+        print(inicio,palabra[inicio],palabra[fin],fin)
+        if palabra[inicio] != palabra[fin]:
+            
+            return False
+        inicio += 1
+        fin -= 1
+    return True
+def reconocerr(palabra):
+    fin = longitud(palabra)
+    for inicio in range(fin//2):
+        print(inicio, palabra[inicio], palabra[fin - 1 - inicio], fin -1 - inicio)
+        if palabra[inicio] != palabra[fin - 1 - inicio]:
+            return False
+    return True
+
+print("reconocer es paralilogramo",reconocerr("reconocer"))
