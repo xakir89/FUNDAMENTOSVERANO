2fc570e23d1cd90f67eb249f837fe21f3e047926
Anderson Lopez <xakir89@gmail.com>
2026-07-23 22:20:34 -0500

    aplicaciones recursividad
    haciendo ejercicios de recursividad y aplicando menus interativos

--- heladeria.py
+++ heladeria.py
@@ -1,41 +1,53 @@
-def inputs(s):    
-    print("="*68)
-    print()
-    print(f"{'HELADERIA DON PEPE':^68}")
-    print()
+def entradas(s):
+    opc = None    
+    try:
+        opc = int(input(s))
+    except ValueError:
+        print(f"Error entrada no valida solo puede ingresar números\n...")
+    return opc
+def confirmar(conChoco,conVaini):
+    print("="*68,'\n')
+    print(f"{'DESEAS OTRO HELADO':^68}")
+    print(f"{'1. SI ':^68}")
+    print(f"{'2. NO ':^68}",'\n')
+    print("="*68,'\n')
+    opc = entradas("Deseas otro helado: ")
+    if opc == 1:
+        return main(conChoco,conVaini)
+    elif opc == 2:
+        return resumen(conChoco,conVaini)
+    else:
+        print('Dato Errado solo 1 o 2\n...')
+        return confirmar(conChoco,conVaini)
+def resumen(conChoco,conVaini):
+        precioChoco = 3
+        precioVaini = 2
+        print("="*68,'\n')
+        print(f"{'RESUMEN COMPRA':^68}"'\n')
+        print(f"{'DESCRIOCION':^30} {'CANTIDAD':^10} {'VALOR U':^10} {'TOTAL':^10}\n")
+        print(f"{'1. Total Chocolate: ':^30} {conChoco:^10} {precioChoco:^10} {'$':^5}{conChoco*precioChoco:^5}")
+        print(f"{'2. Total Vainilla: ':^30} {conVaini:^10} {precioVaini:^10} {'$':^5}{conVaini*precioVaini:^5}\n")
+        print("="*68,'\n')
+        print('saliendo\n...')
+def main(conChoco = 0, conVaini = 0):
+    print("="*68,'\n')
+    print(f"{'HELADERIA DON PEPE':^68}",'\n')
     print(f"{'1. Helado Chocolate':^68}")
     print(f"{'2. Helado Vainilla ':^68}")
-    print(f"{'2. Salir           ':^68}")
-    print()
-    print("="*68)
-    print()
-    try:
-        opc = int(input(s)) 
-        print("...")
-    except ValueError:
-        print(f"Error entrada no valida solo puede ingresar números")
-        print("...")
-    return opc
-
-
-
-        
-def main():
-    entrada = inputs('escoje una opcion: ')
+    print(f"{'3. Salir           ':^68}",'\n')
+    print("="*68,'\n')
+    entrada = entradas('escoje una opcion: ')
     if entrada == 1:
-        print('Helado CHocolate')
-        print('$ 3')        
-        main()
+        print('Helado CHocolate\n$ 3\n...')
+        return confirmar(conChoco + 1, conVaini)
     elif entrada == 2:
-        print('Helado Vainilla')
-        print('$ 2')
-        main()
+        print('Helado Vainilla\n$ 2\n...')
+        return confirmar(conChoco, conVaini + 1)
     elif entrada == 3:
-        print('saliendo')
+        return resumen(conChoco,conVaini)
     else:
         print('dato errado solo hay 3 opciones')
-        main()
-    return entrada
+        return main(conChoco, conVaini)
 
 main()
 
--- recurisividadSuma.py
+++ recurisividadSuma.py
@@ -3,24 +3,77 @@ def suma(numero):
         return 1
     else:
         return numero + suma(numero-1)
-suma(3)
-
 
 def sumarDigitos(numero):
     if numero <= 1:
         return 1
     else:
         return numero%10 + sumarDigitos(numero//10)
-print(sumarDigitos(1422))
 
 def imprimeDigitos(numero):
     if numero <= 0: return
     print (numero, end=' ')
     imprimeDigitos(numero-1)
-imprimeDigitos(5)
-print()
+
 def imprimeDigitosInvertido(numero):
     if numero <= 0: return 
     imprimeDigitosInvertido(numero-1)
     print (numero, end=' ')
-imprimeDigitosInvertido(5)
+
+
+# Ejercicio 2: Calcular la potencia de un número (base^exponente)
+# Pista: Cualquier número elevado a la potencia 0 es 1.
+
+def potencia(base, exponente):
+# Escribe aquí tu Caso Base
+    if exponente==0:
+        return 1
+#Caso Recursivo
+    elif exponente < 0 :
+        return 1 / base * potencia(base, exponente + 1)
+    return base * potencia(base, exponente - 1)
+#print(potencia(2,-2))
+
+# Ejercicio 3: Contar las vocales de una palabra Pista: Evalúa si la primera letra es vocal, 
+# suma 1 (o 0 si no lo es), y pásale a la función el resto de la palabra.
+
+def contar_vocales(palabra):
+    #Caso Base (palabra vacía)
+    if len(palabra) == 0:
+        return 0
+    #Lógica y Caso Recursivo
+    es_vocal = 1 if palabra[0] in "aeiouAEIOU" else 0
+    return es_vocal + contar_vocales(palabra[1:])
+#print(contar_vocales('amore'))
+
+def paginas(libros):
+    if len(libros ) == 0:
+        return 0
+    return libros[0] + paginas(libros[1:])
+#print(paginas([50,100,150,50]))
+
+def factorial2(numero):
+    if numero == 0:
+        return 1
+    return numero * factorial2(numero-1)
+#print(factorial2(100))
+
+# invertir Texto
+def inversionTexto(palabra):
+    if palabra == '' : return ''
+    return inversionTexto(palabra[1:]) + (palabra[0])    
+#print(inversionTexto('paparapa'))
+
+# La Sucesión de Fibonacci: Cada número es la suma de los dos anteriores. (0, 1, 1, 2, 3, 5, 8...).
+# Para calcular el 4to, necesito el 3ro y el 2do a la vez.
+def fibonacci(numero):
+    if numero == 0: return 2 #caso base 1
+    if numero == 1: return 4 #caso base 2
+    return fibonacci(numero-1) + fibonacci(numero-2) 
+def serieFibonacci(numero,iteracion=0):
+    if iteracion > numero: return
+    print(fibonacci(iteracion),end=' ')
+    serieFibonacci(numero, iteracion +1)
+
+
+serieFibonacci(5,)
